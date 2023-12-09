a7b9c4a5fa6f33921f002af3367d40111d5623fe
Guoyuan <130336982+GuoyuanLi123@users.noreply.github.com>
2023-12-09 05:34:26 -0500

    Add files via upload

--- TEASER_SIM_Sync.py
+++ TEASER_SIM_Sync.py
@@ -2,7 +2,7 @@ from SimSyncRegularized import SimSyncReg
 import numpy as np
 from scipy.stats import chi2
 import sys
-sys.path.append('/content/drive/MyDrive/SIM-Sync-MONO/TEASER-plusplus/build/python/teaserpp_python')
+sys.path.insert(0, '/content/drive/MyDrive/SIM-Sync-MONO/TEASER-plusplus/build/python/teaserpp_python')
 import teaserpp_python
 
 def TEASER_SimSync(N, edges, pointclouds, scale_gt = None, reg_lambda=1):
